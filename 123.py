746e49362f391fd2c95442bc017c33fdd7a2832c
whyusoserious <kegont50@gmail.com>
2022-02-14 22:50:07 +0700

    Initial commit

---
+++ 123.py
@@ -0,0 +1,19 @@
+N = int(input())
+A = list(map(int, input().split()))
+N = int(input())
+INF = 10**10
+F = [INF] * (N+1)
+F[0] = 0
+Prev = [0] * (N+1)
+for k in range(1, N+1):
+    for i in range(len(A)):
+        if k-A[i] >= 0 and F[k-A[i]] < F[k]:
+            F[k] = F[k-A[i]]
+            Prev[k]=A[i]
+    F[k] +=1
+if F[N] >= INF:
+    print("impossible")
+else:
+    while N>0:
+        print(Prev[N], end=' ')
+        N-= Prev[N]
