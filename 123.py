f91d0d083ca064bf324b45d157c75b4e070af510
whyusoserious <kegont50@gmail.com>
2022-02-16 17:20:11 +0700

    creating a def for testing in the future

--- 123.py
+++ 123.py
@@ -1,19 +1,24 @@
-N = int(input())
-A = list(map(int, input().split()))
-N = int(input())
-INF = 10**10
-F = [INF] * (N+1)
-F[0] = 0
-Prev = [0] * (N+1)
-for k in range(1, N+1):
-    for i in range(len(A)):
-        if k-A[i] >= 0 and F[k-A[i]] < F[k]:
-            F[k] = F[k-A[i]]
-            Prev[k]=A[i]
-    F[k] +=1
-if F[N] >= INF:
-    print("impossible")
-else:
-    while N>0:
-        print(Prev[N], end=' ')
-        N-= Prev[N]
+def change():
+    N = int(input())
+    A = list(map(int, input().split()))
+    N = int(input())
+
+    INF = 10**10
+    F = [INF] * (N+1)
+    F[0] = 0
+    Prev = [0] * (N+1)
+    for k in range(1, N+1):
+        for i in range(len(A)):
+            if k-A[i] >= 0 and F[k-A[i]] < F[k]:
+                F[k] = F[k-A[i]]
+                Prev[k]=A[i]
+        F[k] +=1
+    if F[N] >= INF:
+        return("impossible")
+    else:
+        amount = ''
+        while N>0:
+            amount += str(Prev[N]) + ' '
+            N-= Prev[N]
+        return (amount)
+change()
